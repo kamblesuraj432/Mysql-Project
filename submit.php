9a6553af4b0945c2231282fe57fc1188227dc6d7
Surajkamble432 <140583784+kamblesuraj432@users.noreply.github.com>
2023-11-23 17:34:11 +0530

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,28 @@
+<?php
+
+$name=$_POST["name"];
+$email=$_POST["email"];
+$website= $_POST["website"];
+$comment=$_POST["comment"];
+$gender=$_POST["gender"];
+
+
+$servername = "localhost";  // Enter Mysql endpoint here
+$username = "root";        // Enter Mysql user name here
+$password = "suraj@123";  // Enter mysql Password here
+$dbname = "suraj";       // Enter mysql database name here
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "INSERT INTO user (name,email,website,comment,gender)VALUES('$name','$email','$website','$comment','$gender')";
+if (mysqli_query($conn, $sql)) {
+  echo "     New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
